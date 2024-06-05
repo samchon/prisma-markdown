7ef60697702fbbd8b42d12d8493f2b8b3c58ea99
Jeongho Nam <samchon.github@gmail.com>
2024-06-05 13:53:55 +0900

    Test multiple schema file case

--- test/index.ts
+++ test/index.ts
@@ -2,18 +2,27 @@ import cp from "child_process";
 import fs from "fs";
 
 const execute = (str: string) => {
-    console.log(`${str}\n`);
-    cp.execSync(str, { stdio: "inherit" });
+  console.log(`${str}\n`);
+  cp.execSync(str, { stdio: "inherit" });
 };
 
 const main = (): void => {
-    process.chdir(__dirname + "/..");
-    execute("npx prisma generate");
+  process.chdir(`${__dirname}/..`);
+  execute("npx prisma generate");
 
-    const directory: string[] = fs
-        .readdirSync(__dirname)
-        .filter((file) => file.endsWith(".prisma"));
-    for (const file of directory)
-        execute(`npx prisma generate --schema test/${file}`);
+  for (const project of fs.readdirSync(`${__dirname}/schemas`)) {
+    const location: string = `${__dirname}/schemas/${project}`;
+    const stat: fs.Stats = fs.statSync(location);
+    if (stat.isDirectory() === false) continue;
+
+    const directory: string[] = fs.readdirSync(location);
+    const count: number = directory.filter((f) => f.endsWith(".prisma")).length;
+    if (count === 0) continue;
+    else if (count === 1)
+      execute(
+        `npx prisma generate --schema test/schemas/${project}/${project}.prisma`,
+      );
+    else execute(`npx prisma generate --schema test/schemas/${project}`);
+  }
 };
 main();
